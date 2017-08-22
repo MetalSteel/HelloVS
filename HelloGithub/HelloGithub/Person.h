4014c79a40aa9d187b0512153a5db9bdfa646ea9
MetalSteel <wanggang225@163.com>
2017-08-22 09:33:58 +0800

    修改Person.h

--- HelloGithub/HelloGithub/Person.h
+++ HelloGithub/HelloGithub/Person.h
@@ -1,13 +1,19 @@
 #pragma once
+#include <iostream>
 
 using namespace std;
 
 class Person
 {
 private:
-	
+	int id;
+	string name;
+	int age;
 public:
 	Person();
+	Person(int id, string name, int age);
 	~Person();
+public:
+
 };
 
